5bb782c8973222cf1d97e9c77c8d53d68e705b0e
RABS <rizkyabdulbakkahshiddiq@gmail.com>
2026-02-25 11:06:24 +0700

    Testing code untuk main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,9 @@
-basic.forever(function () {
-	
-})
+//% weight=100 color=#0fbc11 icon="\uf1b9" block="REST API"
+
+namespace RESTbit_API{
+
+    //% block="say Hello"
+    export function sayHello(): void {
+        basic.showString("Hello")
+    }
+}
