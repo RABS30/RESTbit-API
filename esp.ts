0abccd2038d49e86c1ab5e94f0ae427507f9bc0f
RABS <rizkyabdulbakkahshiddiq@gmail.com>
2026-02-25 13:31:03 +0700

    Tambah code untuk menjalankan REST API di main.ts

--- esp.ts
+++ esp.ts
@@ -11,7 +11,7 @@
  * Blocks for ESP8266 WiFi module.
  */
 //% weight=10 color=#ff8000 icon="\uf1eb" block="ESP8266 WiFi"
-namespace esp8266 {
+namespace restapi {
     // Flag to indicate whether the ESP8266 was initialized successfully.
     let esp8266Initialized = false
 
--- main.ts
+++ main.ts
@@ -1,12 +1,64 @@
 //% weight=10 color=#e0dd10 icon="\uf1eb" block="REST API"
 namespace restapi {
+    const FONNTE_API_URL = "api.fonnte.com"
+    
+    let fonnteMessageSent = false
 
     /**
-     * Show Hello text
+     * Return true if Fonnte message was sent successfully.
      */
-    //% block="say hello"
-    export function sayHello(): void {
-        basic.showString("Hello")
+    //% subcategory="Fonnte"
+    //% block="Fonnte message sent"
+    export function isFonnteMessageSent(): boolean {
+        return fonnteMessageSent
     }
 
+    /**
+     * Send WhatsApp message via Fonnte
+     */
+    //% subcategory="Fonnte"
+    //% block="send WA Fonnte target %target message %message"
+    export function sendFonnteMessage(target: string, message: string) {
+
+        fonnteMessageSent = false
+
+        if (isWifiConnected() == false) return
+
+        // Open SSL connection
+        if (sendCommand("AT+CIPSTART=\"SSL\",\"" + FONNTE_API_URL + "\",443", "OK", 10000) == false) return
+
+        // Prepare POST body
+        let body = "target=" + target + "&message=" + message
+
+        // Construct HTTP POST request
+        let data = "POST /send HTTP/1.1\r\n"
+        data += "Host: " + FONNTE_API_URL + "\r\n"
+        data += "Authorization: uvMKVqTmBjdrMoryFZBM\r\n"
+        data += "Content-Type: application/x-www-form-urlencoded\r\n"
+        data += "Content-Length: " + body.length + "\r\n\r\n"
+        data += body
+
+        // Inform ESP8266 how many bytes to send
+        sendCommand("AT+CIPSEND=" + data.length)
+
+        // Send request
+        sendCommand(data)
+
+        // Check if data sent successfully
+        if (getResponse("SEND OK", 3000) == "") {
+            sendCommand("AT+CIPCLOSE", "OK", 2000)
+            return
+        }
+
+        // Check HTTP success (status 200)
+        let response = getResponse("200 OK", 5000)
+        if (response == "") {
+            sendCommand("AT+CIPCLOSE", "OK", 2000)
+            return
+        }
+
+        sendCommand("AT+CIPCLOSE", "OK", 2000)
+
+        fonnteMessageSent = true
+    }
 }
