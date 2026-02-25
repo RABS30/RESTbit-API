e7aec70b219940c6d455afb3caaba8a301cb0d81
RABS <rizkyabdulbakkahshiddiq@gmail.com>
2026-02-25 12:29:40 +0700

    Buat code untuk menginisialisasi dan mengoneksikan wifi ESP8266

--- esp.ts
+++ esp.ts
@@ -1,12 +1,88 @@
-//% weight=10 color=#e0dd10 icon="\uf1eb" block="ESP"
-namespace esp {
+//% weight=10 color=#e0dd10 icon="\uf1eb" block="ESP8266"
+namespace esp8266 {
+
+    let _rxPin: SerialPin
+    let _txPin: SerialPin
 
     /**
-     * Show Hello text
+     * Initialize ESP8266 serial connection
      */
-    //% block="say hello"
-    export function sayHello(): void {
-        basic.showString("Hello")
+    //% block="init esp8266 RX %rx TX %tx baud %baud"
+    export function init(rx: SerialPin, tx: SerialPin, baud: BaudRate): void {
+        _rxPin = rx
+        _txPin = tx
+        serial.redirect(tx, rx, baud)
+        basic.pause(2000)
+        sendCommand("AT")
+    }
+
+    /**
+     * Send raw AT command
+     */
+    //% block="send AT command %cmd"
+    export function sendCommand(cmd: string): string {
+        serial.writeString(cmd + "\r\n")
+        basic.pause(2000)
+        return serial.readString()
+    }
+
+    /**
+     * Connect to WiFi
+     */
+    //% block="connect wifi ssid %ssid password %password"
+    export function connectWiFi(ssid: string, password: string): string {
+        sendCommand("AT+CWMODE=1")
+        basic.pause(1000)
+        serial.writeString("AT+CWJAP=\"" + ssid + "\",\"" + password + "\"\r\n")
+        basic.pause(6000)
+        return serial.readString()
+    }
+
+    /**
+     * Send HTTP GET request
+     */
+    //% block="http get host %host path %path"
+    export function httpGet(host: string, path: string): string {
+        sendCommand("AT+CIPMUX=0")
+        serial.writeString("AT+CIPSTART=\"TCP\",\"" + host + "\",80\r\n")
+        basic.pause(3000)
+
+        let request = "GET " + path + " HTTP/1.1\r\nHost: " + host + "\r\n\r\n"
+
+        serial.writeString("AT+CIPSEND=" + request.length + "\r\n")
+        basic.pause(2000)
+        serial.writeString(request)
+
+        basic.pause(5000)
+        return serial.readString()
     }
 
 }
+
+// namespace esp {
+//     // Variable untuk mengetahui apakah esp8266 berhasil terinisialisasi
+//     let espInitialized = false;
+
+//     // Buffer untuk menerima data dari UART
+//     let dataCome = ""
+
+//     export function sendCommand(command: string, response: string = null, timeout: number = 100): boolean {
+//         // Jeda sesaat untuk menunggu command sebelumnya
+//         basic.pause(10);
+
+//         // Mengosongkan buffer data yang diterima UART
+//         serial.readString()
+//         dataCome = ""
+
+//         // Mengirim command dan diakhiri dengan "\r\n"
+//         serial.writeString(command + "\r\n")
+
+//         //
+        
+
+//         return
+//     }
+
+// }
+
+
