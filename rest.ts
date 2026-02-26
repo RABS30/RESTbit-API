ef749d83cf394970e630fc24041d86e8c9627ce4
RABS <rizkyabdulbakkahshiddiq@gmail.com>
2026-02-26 16:36:37 +0700

    Percobaan rest api menggunakan file rest

--- rest.ts
+++ rest.ts
@@ -2,25 +2,25 @@
 namespace restapi {
     const FONNTE_API_URL = "api.fonnte.com"
 
-    let fonnteMessageSent = false
+    let dataRequestSent = false
 
     /**
      * Return true if Fonnte message was sent successfully.
      */
-    //% subcategory="Fonnte"
-    //% block="Fonnte message sent"
-    export function isFonnteMessageSent(): boolean {
-        return fonnteMessageSent
+    //% subcategory="REST API"
+    //% block="Request message sent"
+    export function isRequestSend(): boolean {
+        return dataRequestSent
     }
 
     /**
      * Send WhatsApp message via Fonnte
      */
-    //% subcategory="Fonnte"
-    //% block="send WA Fonnte target %target message %message"
-    export function sendFonnteMessage(target: string, message: string) {
+    //% subcategory="REST API"
+    //% block="Send request to number %number message %message"
+    export function sendRequest(number: string, message: string, url: string) {
 
-        fonnteMessageSent = false
+        dataRequestSent = false
 
         if (isWifiConnected() == false) return
 
@@ -28,14 +28,14 @@ namespace restapi {
         sendCommand("AT+CIPSSLCCONF=0")
         
         // Open SSL connection
-        if (sendCommand("AT+CIPSTART=\"SSL\",\"" + FONNTE_API_URL + "\",443", "OK", 10000) == false) return
+        if (sendCommand("AT+CIPSTART=\"SSL\",\"" + url + "\",443", "OK", 10000) == false) return
 
         // Prepare POST body
-        let body = "target=" + target + "&message=" + message
+        let body = "target=" + number + "&message=" + message
 
         // Construct HTTP POST request
         let data = "POST /send HTTP/1.1\r\n"
-        data += "Host: " + FONNTE_API_URL + "\r\n"
+        data += "Host: " + url + "\r\n"
         data += "Authorization: uvMKVqTmBjdrMoryFZBM\r\n"
         data += "Content-Type: application/x-www-form-urlencoded\r\n"
         data += "Content-Length: " + body.length + "\r\n\r\n"
@@ -62,6 +62,6 @@ namespace restapi {
 
         sendCommand("AT+CIPCLOSE", "OK", 2000)
 
-        fonnteMessageSent = true
+        dataRequestSent = true
     }
 }
